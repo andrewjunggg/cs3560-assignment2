214a90d5aada986353352deabb331421c94ceb28
andrewjunggg <andrewjunggg@gmail.com>
2023-06-24 20:34:18 -0700

    Initial commit

---
+++ src/main/java/com/andrewjunggg/Driver.java
@@ -0,0 +1,13 @@
+package com.andrewjunggg;
+
+/**
+ * Hello world!
+ *
+ */
+public class Driver 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
