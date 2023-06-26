6bae5c385ce292df22cdebdacd05fe4573338f87
andrewjunggg <andrewjunggg@gmail.com>
2023-06-25 18:09:39 -0700

    Add manage button skeletons

--- src/main/java/com/andrewjunggg/ui/ManagePanel.java
+++ src/main/java/com/andrewjunggg/ui/ManagePanel.java
@@ -1,15 +1,24 @@
 package com.andrewjunggg.ui;
 
-import java.awt.BorderLayout;
 import java.awt.Dimension;
 
 import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 public class ManagePanel extends JPanel {
     public ManagePanel() {
-        setLayout(new BorderLayout());
-        setPreferredSize(new Dimension(500, 500));
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        setPreferredSize(new Dimension(300, 300));
         setBorder(BorderFactory.createTitledBorder("Manage"));
+
+        JButton addUserButton = new JButton("Add user");
+        JButton addGroupButton = new JButton("Add group");
+        JButton userViewButton = new JButton("Open user view");
+
+        add(addUserButton);
+        add(addGroupButton);
+        add(userViewButton);
     }
 }
